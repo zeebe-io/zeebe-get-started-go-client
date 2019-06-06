27de53a91b72a61dab283695d0872b314c41f0f1
Sebastian Menski <sebastian.menski@camunda.com>
2019-06-06 21:02:22 +0200

    chore(project): update to Zeebe 0.18.0

--- example-3/main.go
+++ example-3/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/zeebe-io/zeebe/clients/go/zbc"
 )
 
@@ -14,10 +15,10 @@ func main() {
 	}
 
 	// After the workflow is deployed.
-	payload := make(map[string]interface{})
-	payload["orderId"] = "31243"
+	variables := make(map[string]interface{})
+	variables["orderId"] = "31243"
 
-	request, err := client.NewCreateInstanceCommand().BPMNProcessId("order-process").LatestVersion().VariablesFromMap(payload)
+	request, err := client.NewCreateInstanceCommand().BPMNProcessId("order-process").LatestVersion().VariablesFromMap(variables)
 	if err != nil {
 		panic(err)
 	}
