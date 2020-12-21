979fecf1ad4f29032a8f184c1c6057dcb800b73c
pihme <pihme@users.noreply.github.com>
2020-12-21 16:33:42 +0100

    chore(src): adjust examples to work with Camunda Cloud

--- src/example-1.go
+++ src/example-1.go
@@ -5,14 +5,40 @@ import (
 	"fmt"
 	"github.com/zeebe-io/zeebe/clients/go/pkg/pb"
 	"github.com/zeebe-io/zeebe/clients/go/pkg/zbc"
+	"os"
 )
 
-const BrokerAddr = "0.0.0.0:26500"
+const ZeebeAddr = "0.0.0.0:26500"
 
+/*
+Sample application that connects to a cluster on Camunda Cloud, or a locally deployed cluster.
+
+When connecting to a cluster in Camunda Cloud, this application assumes that the following
+environment variables are set:
+
+ZEEBE_ADDRESS
+ZEEBE_CLIENT_ID
+ZEEBE_CLIENT_SECRET
+ZEEBE_AUTHORIZATION_SERVER_URL
+
+Hint: When you create client credentials in Camunda Cloud you have the option
+to download a file with the lines above filled out for you.
+
+When connecting to a local cluster or node, this application assumes default port and no
+authentication or encryption enabled.
+*/
 func main() {
+	gatewayAddr := os.Getenv("ZEEBE_ADDRESS")
+	var plainText bool
+
+	if gatewayAddr == "" {
+		gatewayAddr = ZeebeAddr
+		plainText = true
+	}
+
 	zbClient, err := zbc.NewClient(&zbc.ClientConfig{
-		GatewayAddress:         BrokerAddr,
-		UsePlaintextConnection: true,
+		GatewayAddress:         gatewayAddr,
+		UsePlaintextConnection: plainText,
 	})
 
 	if err != nil {
